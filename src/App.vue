10ecc07a6f0a725c1090cdde0053a73330d650bf
samman <monem7242@gmail.com>
2020-06-25 06:06:07 +0200

    create todo, item and tabs components

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
     <div id="app">
         <Header></Header>
-            <Footer></Footer>
+        <Todo></Todo>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
     import Header from './components/header.vue'
     import Footer from './components/footer.jsx'
+    import Todo from './components/todo.vue'
 
     export default {
         components: {
             Header,
             Footer,
+            Todo,
         }
     }
 </script>
 
 <style lang="stylus" scoped>
-    #app{
+    #app {
         position absolute
         left 0
         right 0
         top 0
         bottom 0
     }
-    #cover{
+
+    #cover {
         position absolute
         left 0
         top 0
